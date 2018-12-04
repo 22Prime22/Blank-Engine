0de767654f27444e20528776e21d6e8180b52ad2
22Prime22 <44883351+22Prime22@users.noreply.github.com>
2018-12-04 10:37:17 +0000

    Create MainState.java

---
+++ BlankEngine/state/MainState.java
@@ -0,0 +1,24 @@
+package blankengine.state;
+
+import java.awt.Graphics;
+import blankengine.Handler;
+
+public class MainState extends State {
+
+    public MainState(final Handler handler) {
+        super(handler);
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void render(Graphics g) {
+    }
+
+    @Override
+    public void init() {
+    }
+    
+}
